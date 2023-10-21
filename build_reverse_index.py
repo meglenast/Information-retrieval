22c51fa8f76f6d514103e274617cfa9ba40b725d
meglenast <meglena.stefanova98@gmail.com>
2023-10-21 22:48:20 +0300

    feat: adding utils function for working with corpuses

--- build_reverse_index.py
+++ build_reverse_index.py
@@ -12,12 +12,27 @@ Options:
 from docopt import docopt
 
 from utils import reverse_index as RI
+from utils import trie as T
+from utils import utils
+from nltk.corpus import PlaintextCorpusReader
+
 if __name__ == '__main__':
     arguments = docopt(__doc__)
     files = arguments['FILE']
-    corpus = arguments['<corpus>']
+    corpus_root = arguments['<corpus>']
     output_file = arguments['<output_file>']
 
-    r = RI.ReverseIndex(files) if files != [] else RI.ReverseIndex(corpus)
+    # r = RI.ReverseIndex(files) if files != [] else RI.ReverseIndex(corpus)
+    # trie = T.Trie()
+    # trie.add_word('megie')
+    # trie.display()
+    
+    corpus = PlaintextCorpusReader(corpus_root, '.*\.txt')
+    fileNames = corpus.fileids()
+    classesSet = set( [ file[:file.find('/')] for file in fileNames ] )
+    classes = sorted(list(classesSet - {'Z','D-Society'}))
 
-    print(res)
+    print(classes)
+
+    fullClassCorpus = [ [ corpus.words(file) for file in fileNames if file.find(c+'/')==0 ] for c in classes ]
+    print(fullClassCorpus[0][0][14])
--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,6 @@
+import random
+from nltk.corpus import PlaintextCorpusReader
+
 def serialize_text_into_set(file: str):
     print(f"Tokenizing file {file}")
     f = open(file, "r")
@@ -13,3 +16,23 @@ def print_documents(corpus, file_names, ids):
         for word in text:
             print(word,end=' ')
         print('\n')
+
+# Splits corpus into training set and testing set
+def split_class_corpus(corpus, test_fraction = 0.1):
+    test_corpus = []
+    train_corpus = []
+    random.seed(42)
+    for class_id in range(len(corpus)):
+        class_lst = corpus[class_id]
+        random.shuffle(class_lst)
+        test_corpus_size = int(len(class_lst) * test_fraction)
+        test_corpus.append(class_lst[:test_corpus_size])
+        train_corpus.append(class_lst[test_corpus_size:])
+    return test_corpus, train_corpus
+
+def read_corpus(corpus_root: str):
+    return PlaintextCorpusReader(corpus_root, '.*\.txt')
+
+# Listing the directories names in corpus
+def list_file_names(corpus: PlaintextCorpusReader):
+    return corpus.fileids()

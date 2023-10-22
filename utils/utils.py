31b2c438aaca869b8659c576ba45d6ccb95567cb
meglenast <meglena.stefanova98@gmail.com>
2023-10-22 09:08:24 +0300

    fix: fix natural language corpus reading functions

--- utils/utils.py
+++ utils/utils.py
@@ -18,21 +18,28 @@ def print_documents(corpus, file_names, ids):
         print('\n')
 
 # Splits corpus into training set and testing set
-def split_class_corpus(corpus, test_fraction = 0.1):
+def split_class_corpus(corpus_per_classes: dict, test_fraction = 0.1):
     test_corpus = []
     train_corpus = []
     random.seed(42)
-    for class_id in range(len(corpus)):
-        class_lst = corpus[class_id]
+    for class_id in corpus_per_classes.keys():
+        class_lst = corpus_per_classes[class_id]
         random.shuffle(class_lst)
         test_corpus_size = int(len(class_lst) * test_fraction)
         test_corpus.append(class_lst[:test_corpus_size])
         train_corpus.append(class_lst[test_corpus_size:])
     return test_corpus, train_corpus
 
+# Reading the names of all files in the corpus
 def read_corpus(corpus_root: str):
     return PlaintextCorpusReader(corpus_root, '.*\.txt')
 
 # Listing the directories names in corpus
 def list_file_names(corpus: PlaintextCorpusReader):
     return corpus.fileids()
+
+def list_classes(corpus_dirs: [str]):
+    return set([dir.split('/')[0] for dir in corpus_dirs])
+
+def extract_words_per_classes(corpus: PlaintextCorpusReader, file_names: [], classes):
+    return { c: [ corpus.words(file) for file in file_names if file.startswith(c+'/') ] for c in classes }
